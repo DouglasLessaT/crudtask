9abbbf6f763ef6ae63e5d0b81f1d12b6d7b22aaa
MAGOCIZENTO <douglaslessat@gmail.com>
2024-03-23 23:22:04 -0300

    first commit

---
+++ src/main/java/com/br/crudtask/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.br.crudtask.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.crudtask.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ src/test/java/com/br/crudtask/CrudtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.crudtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
